681ea5dbcd0ce87cfe72cd812741cf8dc8207428
Fahad AlGarni <fahad.a.s.algarni@gmail.com>
2021-12-10 18:19:28 +0300

    Imported imguifiledialog

--- OpenGLRenderer/src/render/PropertyPanel.cpp
+++ OpenGLRenderer/src/render/PropertyPanel.cpp
@@ -1,16 +1,31 @@
 #include <pch.h>
 #include "PropertyPanel.h"
+#include <3rdparty/imguifiledialog/ImGuiFileDialog.h>
 
 void PropertyPanel::Render(SceneView* m_Sceneview)
 {
     ImGui::Begin("Window");
     ImGui::Text("Hello World!");
 
-    ImGuiIO& io = ImGui::GetIO();    
+    ImGuiIO& io = ImGui::GetIO();
 
+    // open Dialog Simple
     if (ImGui::Button("Set Mesh"))
+        ImGuiFileDialog::Instance()->OpenDialog("SetMesh", "Choose File", ".obj", ".");
+
+    // display
+    if (ImGuiFileDialog::Instance()->Display("SetMesh"))
     {
-        m_SetMeshCallback("res/models/quad.obj");
+        // action if OK
+        if (ImGuiFileDialog::Instance()->IsOk())
+        {
+            std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
+            std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
+            m_SetMeshCallback(filePathName);
+        }
+
+        // close
+        ImGuiFileDialog::Instance()->Close();
     }
 
     ImGui::SliderFloat3("Position", &m_Sceneview->m_Position.x, -5.0f, 5.0f);
